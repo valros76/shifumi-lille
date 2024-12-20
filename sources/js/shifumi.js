be249e86f65e6f292e4a927713531deada3dff79
valros76 <valerian.dufrene@gmail.com>
2024-12-20 13:03:01 +0100

    Code complet

--- sources/js/shifumi.js
+++ sources/js/shifumi.js
@@ -15,6 +15,97 @@ window.addEventListener("DOMContentLoaded", () => {
     return action;
   }
 
+  const generateRandomAction = () => signs[Math.floor(Math.random() * signs.length)];
+
+  const verifyGame = () => {
+    const playerSign = document.querySelector(".player-sign").textContent;
+    const iaSign = document.querySelector(".ia-sign").textContent;
+
+    if (playerSign === iaSign) return null;
+
+    if (
+      (playerSign === "✊" && iaSign === "✌️")
+      || (playerSign === "✌️" && iaSign === "✋")
+      || (playerSign === "✋" && iaSign === "✊")
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const setResultModal = (resultText, headTitle) => {
+    const resultModal = document.createElement("div");
+    resultModal.id = "resultModal";
+    resultModal.className = "result-modal result-show";
+
+    const resultParagraph = document.createElement("p");
+    resultParagraph.className = "result";
+    resultParagraph.textContent = resultText;
+
+    const reloadButton = document.createElement("button");
+    reloadButton.id = "reloadGame";
+    reloadButton.textContent = "Rejouer ?";
+
+    resultModal.append(resultParagraph, reloadButton);
+    boardGamePlay.appendChild(resultModal);
+
+    const mainHeadTitle = document.querySelector(".main-head-title");
+    mainHeadTitle.textContent = headTitle;
+
+    reloadButton.addEventListener("click", () => {
+      gameEnd = false;
+      mainHeadTitle.textContent = "Pierre - Feuille - Ciseaux";
+      resultModal.remove();
+      document.querySelector("#launchGameModal").classList.add("launch-game-show");
+    });
+  }
+
+  const endGame = (playerWon) => {
+    gameStart = false;
+    gameEnd = true;
+
+    switch (playerWon) {
+      case true:
+        setResultModal("🎯 Victoire 🎯", "🎉🎉🎉");
+        break;
+      case false:
+        setResultModal("💀 Défaite 💀", "☠️☠️☠️");
+        break;
+      default:
+        setResultModal("✖️ Match nul ✖️", "❌❌❌");
+        break;
+    }
+  }
+
+  const createComputerElements = () => {
+    const iaSigns = document.createElement("div");
+    iaSigns.className = "ia-signs";
+
+    const description = document.createElement("p");
+    description.className = "ia-signs-description";
+    description.textContent = "Action de l'ordinateur";
+
+    const action = createSignElement(generateRandomAction(), "ia");
+    description.textContent = action.dataset.sign;
+
+    iaSigns.append(description, action);
+    boardGamePlay.appendChild(iaSigns);
+
+    const mainHeadTitle = document.querySelector(".main-head-title");
+    let countdownInit = 3;
+    const countdown = setInterval(() => {
+      mainHeadTitle.textContent = countdownInit;
+      countdownInit--;
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(countdown);
+      mainHeadTitle.textContent = "0";
+      endGame(verifyGame());
+    }, 4000);
+  }
+
   const createPlayerElements = () => {
     const playerSigns = document.createElement("div");
     playerSigns.className = "player-signs";
@@ -34,7 +125,29 @@ window.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         description.textContent = e.target.dataset.sign;
         description.classList.add("freeze");
+
+        document.querySelectorAll(".player-sign").forEach(action => {
+          if (action != e.target) action.remove();
+          action.style.pointerEvent = "none";
+        });
+
+        if (!document.querySelector(".ia-sign")) {
+          createComputerElements();
+        }
       });
+
+      action.addEventListener("mouseover", () => {
+        if (!description.classList.contains("freeze")) {
+          description.textContent = action.dataset.sign;
+        }
+      });
+
+      action.addEventListener("mouseleave", () => {
+        if (!description.classList.contains("freeze")) {
+          description.textContent = "Choisir votre action";
+        }
+      });
+
     });
   }
 
@@ -45,7 +158,7 @@ window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#loadGame").addEventListener("click", e => {
     e.preventDefault();
 
-    if(!gameStart){
+    if (!gameStart) {
       document.querySelector("#launchGameModal").classList.remove("launch-game-show");
       cleanBoardGamePlay();
       createPlayerElements();
@@ -54,5 +167,5 @@ window.addEventListener("DOMContentLoaded", () => {
     gameStart = !gameStart;
 
   });
-  
+
 });
